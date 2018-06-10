109551c679ffd758e78b3d0242dfbf9deb265a28
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2018-06-09 22:18:42 -0500

    formated code

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-
-import { NativeModules } from 'react-native';
+import { NativeModules } from "react-native";
 
 const { RNPalette } = NativeModules;
 
 const TAG = "[Palette]";
 export default class Palette {
-    static getNamedSwatchesFromUrl(url) {
-    __DEV__ && console.debug(TAG, "getNamedSwatchesFromUrl",url);
-      return RNPalette.getNamedSwatchesFromUrl(url);
-    }
-  
-    static getAllSwatchesFromUrl(url) {
-        __DEV__ && console.debug(TAG, "getAllSwatchesFromUrl",url);
-      return RNPalette.getAllSwatchesFromUrl(url);
-    }
-  
-    static getNamedSwatches(path) {
-        __DEV__ && console.debug(TAG, "getNamedSwatches",path);
-      return RNPalette.getNamedSwatches(path);
-    }
-  
-    static getAllSwatches(path) {
-        __DEV__ && console.debug(TAG, "getAllSwatches",path);
-      return RNPalette.getAllSwatches(path);
-    }
+  static getNamedSwatchesFromUrl(url) {
+    __DEV__ && console.debug(TAG, "getNamedSwatchesFromUrl", url);
+    return RNPalette.getNamedSwatchesFromUrl(url);
   }
-  
+
+  static getAllSwatchesFromUrl(url) {
+    __DEV__ && console.debug(TAG, "getAllSwatchesFromUrl", url);
+    return RNPalette.getAllSwatchesFromUrl(url);
+  }
+
+  static getNamedSwatches(path) {
+    __DEV__ && console.debug(TAG, "getNamedSwatches", path);
+    return RNPalette.getNamedSwatches(path);
+  }
+
+  static getAllSwatches(path) {
+    __DEV__ && console.debug(TAG, "getAllSwatches", path);
+    return RNPalette.getAllSwatches(path);
+  }
+}
